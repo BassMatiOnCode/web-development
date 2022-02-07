a4945e3fac4b31d53cf6624bb9648aaeab4772f8
BassmatiOnMusic <bassmatiOnMusic@freenet.de>
2022-02-07 21:48:14 +0100

    .

--- start-dev.bat
+++ start-dev.bat
@@ -1,4 +1,4 @@
 d:\progs\VS2022\Common7\IDE\devenv.com "D:\github\web-design-toolbox"
 start /min deno-file-server
-start "msedge.exe" "http://localhost:4507/sticky-headers/sticky-headers.htm"
+start "msedge.exe" "http://localhost:4507/collapsible-5/collapsible-5.htm"
 
