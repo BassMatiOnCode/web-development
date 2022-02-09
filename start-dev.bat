925c2be2cbae38e5eda77ac0cfadaef82e409a8c
BassmatiOnMusic <bassmatiOnMusic@freenet.de>
2022-02-09 09:32:06 +0100

    invalid symlinks break github build process

--- start-dev.bat
+++ start-dev.bat
@@ -1,4 +1,5 @@
-d:\progs\VS2022\Common7\IDE\devenv.com "D:\github\web-design-toolbox"
+d:\progs\VS2022\Common7\IDE\devenv.com "D:\github\bassmati-on-code\web-design-toolbox"
+cd docs
 start /min deno-file-server
-start "msedge.exe" "http://localhost:4507/collapsible-5/collapsible-5.htm"
+start msedge.exe "http://localhost:4507/index.htm"
 
